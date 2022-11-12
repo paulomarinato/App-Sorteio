a0af0738b521285e5bd133ec70cb0ece206f4fab
paulomarinato <marinatopaulo@gmail.com>
2022-11-12 20:25:52 -0300

    Finaliazação das funções e link de botão e textview

--- app/src/main/java/com/criandoapps/sorteio/MainActivity.java
+++ app/src/main/java/com/criandoapps/sorteio/MainActivity.java
@@ -3,6 +3,10 @@ package com.criandoapps.sorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void sortear(View view){
+        TextView texto = findViewById(R.id.resultado);
+        int x = new Random().nextInt(101);
+        texto.setText( "Numero é: " + x );
+
     }
 
 }
